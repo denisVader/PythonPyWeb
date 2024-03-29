c8d8458f0bbd551932a4e5ea5ec0e7229984f397
denisVader <vadea@mail.ru>
2024-03-29 21:12:55 +0300

    3bd

--- apps/db_train/models.py
+++ apps/db_train/models.py
@@ -107,8 +107,7 @@ class Author(models.Model):
             self.age = today.year - self.date_birth.year - additional_year  # Перезаписываем значение
         super().save(*args, **kwargs)
 
-    def __dir__(self):
-        return f"{self.username} - {self.last_name} {self.first_name.upper()[0]}.{self.middle_name.upper()[0]}."
+
 
     class Meta:
         verbose_name = "Автор"
@@ -121,3 +120,30 @@ class Author(models.Model):
         if self.first_name and self.middle_name:
             initials = f"{self.first_name.upper()[0]}.{self.middle_name.upper()[0]}."
         return f"{self.username} - {self.last_name} {initials}"
+
+class AuthorProfile(models.Model):
+    author = models.OneToOneField('Author', on_delete=models.CASCADE)
+    stage = models.IntegerField(default=0,
+                                blank=True,
+                                verbose_name="Стаж",
+                                help_text="Стаж в годах")
+
+    def __str__(self):
+        return f"Автор: {self.author.username}; Стаж: {self.stage} лет"
+
+class Entry(models.Model):
+    text = models.TextField(verbose_name="Текст статьи",
+                            )
+    author = models.ForeignKey("Author", on_delete=models.CASCADE, related_name='entries')
+    tags = models.ManyToManyField("Tag", related_name='entries')
+
+    def __str__(self):
+        return f"Статья {self.tags.all().values_list('name')}"
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50,
+                            verbose_name="Название",
+                            )
+
+    def __str__(self):
+        return f"Теги: {self.name}"
--- apps/db_train_alternative/admin.py
+++ apps/db_train_alternative/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Author, Tag, AuthorProfile, Entry
+from .models import Blog, Author, Entry, Tag, AuthorProfile
 
+admin.site.register(Blog)
 admin.site.register(Author)
+admin.site.register(Entry)
 admin.site.register(Tag)
 admin.site.register(AuthorProfile)
-admin.site.register(Entry)
