08894df8cd6039399904f7c558771149ee4833cd
denisVader <vadera@mail.ru>
2024-04-12 18:15:33 +0300

    bd9

--- apps/api/views.py
+++ apps/api/views.py
@@ -5,6 +5,47 @@ from django.views.decorators.csrf import \
     csrf_exempt  # Чтобы post, put, patch, delete не требовали csrf токена (небезопасно)
 from apps.db_train_alternative.models import Author
 from .serializers import AuthorSerializer
+from django.http import Http404
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import (RetrieveModelMixin, ListModelMixin,
+                                   CreateModelMixin, UpdateModelMixin, DestroyModelMixin)
+from .serializers import AuthorModelSerializer
+
+
+class AuthorGenericAPIView(GenericAPIView, RetrieveModelMixin, ListModelMixin, CreateModelMixin, UpdateModelMixin,
+                           DestroyModelMixin):
+    queryset = Author.objects.all()
+    serializer_class = AuthorModelSerializer
+
+    def get(self, request, *args, **kwargs):
+        if kwargs.get(self.lookup_field):  # если был передан id или pk
+            # возвращаем один объект
+            return self.retrieve(request, *args, **kwargs)
+        # Иначе возвращаем список объектов
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        if kwargs.get(self.lookup_field):
+            try:
+                # возвращаем один объект
+                return self.retrieve(request, *args, **kwargs)
+            except Http404:
+                return Response({'message': 'Автор не найден'}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            # Иначе возвращаем список объектов
+            return self.list(request, *args, **kwargs)
 
 
 class AuthorAPIView(APIView):
